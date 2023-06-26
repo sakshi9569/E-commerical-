43916117ef653691fc8742872442bcbe97b912be
sakshi rai <106828740+sakshi9569@users.noreply.github.com>
2023-06-26 14:26:35 +0530

    Update categoryitem.jsx

--- src/components/categoryitem.jsx
+++ src/components/categoryitem.jsx
@@ -1,21 +1,55 @@
 import styled from "styled-components";
-import { categories } from "../data";
 import { mobile } from "../responsive";
-import CategoryItem from "./CategoryItem";
 
 const Container = styled.div`
-  display: flex;
-  padding: 20px;
-  justify-content: space-between;
-  ${mobile({ padding: "0px", flexDirection:"column" })}
+  flex: 1;
+  margin: 3px;
+  height: 70vh;
+  position: relative;
 `;
 
-const Categories = () => {
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  ${mobile({ height: "20vh" })}
+`;
+
+const Info = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Title = styled.h1`
+    color:white;
+    margin-bottom: 20px;
+`;
+
+const Button = styled.button`
+    border:none;
+    padding: 10px;
+    background-color: white;
+    color:gray;
+    cursor: pointer;
+    font-weight: 600;
+`;
+
+const CategoryItem = ({ item }) => {
   return (
     <Container>
-      {categories.map((item) => (
-        <CategoryItem item={item} key={item.id} />
-      ))}
+      <Image src={item.img} />
+      <Info>
+        <Title>{item.title}</Title>
+        <Button>SHOP NOW</Button>
+      </Info>
     </Container>
   );
 };
+
